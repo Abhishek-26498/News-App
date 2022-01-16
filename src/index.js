6c551e886f647e33fa556a34ee67c86c22a99b36
Abhishek-26498 <dhiman26abhishek@gmail.com>
2022-01-16 12:39:53 -0800

    news app[

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import News from './News';
+
+
+ReactDOM.render(
+    <>
+        <News />
+    </>
+  ,document.getElementById('root')
+);
+
+
